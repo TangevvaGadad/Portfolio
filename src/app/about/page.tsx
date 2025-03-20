381dc3ddf3727e34b20f62d35b0f5214627043af
Ashwinigadad <ashwinigadad3@gmail.com>
2025-03-20 22:40:54 +0530

    projects

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -115,10 +115,6 @@ export default function About() {
             horizontal="center"
           >
             <Avatar src={person.avatar} size="xl" />
-            <Flex gap="8" vertical="center">
-              <Icon onBackground="accent-weak" name="globe" />
-              {person.location}
-            </Flex>
             {person.languages.length > 0 && (
               <Flex wrap gap="8">
                 {person.languages.map((language, index) => (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 
-import { Heading, Flex, Text, Button, Avatar, RevealFx, Arrow, Column } from "@/once-ui/components";
-import { Projects } from "@/components/work/Projects";
-
-import { baseURL, routes } from "@/app/resources";
-import { home, about, person, newsletter } from "@/app/resources/content";
-import { Mailchimp } from "@/components";
-import { Posts } from "@/components/blog/Posts";
+import { Heading, Flex, Text, Button, Avatar, RevealFx, Column } from "@/once-ui/components";
+import { baseURL } from "@/app/resources";
+import { home, about, person } from "@/app/resources/content";
 
 export default function Home() {
   return (
@@ -33,19 +29,23 @@ export default function Home() {
           }),
         }}
       />
-      <Column fillWidth paddingY="l" gap="m">
-        <Column maxWidth="s">
-          <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="m">
-            <Heading wrap="balance" variant="display-strong-l">
+
+      <Column fillWidth paddingY="l" gap="m" horizontal="center">
+        <Column maxWidth="s" horizontal="center">
+          <RevealFx translateY="4" fillWidth horizontal="center" paddingBottom="m">
+            <Heading wrap="balance" variant="display-strong-l" align="center">
               {home.headline}
             </Heading>
           </RevealFx>
-          <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="m">
-            <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl">
+          <RevealFx translateY="8" delay={0.2} fillWidth horizontal="center" paddingBottom="m">
+            <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl" align="center">
               {home.subline}
             </Text>
           </RevealFx>
-          <RevealFx translateY="12" delay={0.4} horizontal="start">
+          <RevealFx translateY="12" delay={0.4} horizontal="center" paddingBottom="l">
+            <Avatar src={person.avatar} size="xl" />
+          </RevealFx>
+          <RevealFx translateY="16" delay={0.6} horizontal="center">
             <Button
               id="about"
               data-border="rounded"
@@ -55,24 +55,15 @@ export default function Home() {
               arrowIcon
             >
               <Flex gap="8" vertical="center">
-                {about.avatar.display && (
-                  <Avatar
-                    style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
-                    src={person.avatar}
-                    size="m"
-                  />
-                )}
                 {about.title}
               </Flex>
             </Button>
           </RevealFx>
         </Column>
       </Column>
-      {/* <RevealFx translateY="16" delay={0.6}>
-        <Projects range={[1, 1]} />
-      </RevealFx> */}
 
-      {/* <Projects range={[2]} /> */}
+      {/* Optional future sections */}
+      {/* <Projects range={[1, 1]} /> */}
       {/* {newsletter.display && <Mailchimp newsletter={newsletter} />} */}
     </Column>
   );
