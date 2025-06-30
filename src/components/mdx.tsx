e1fe4a6316efcffbc20b9f0f1d9a61f1a11eb9db
TangevvaGadad <ashwinigadad3@gmail.com>
2025-06-30 11:46:57 +0530

    modified

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -89,7 +89,7 @@ function slugify(str: string): string {
     .toLowerCase()
     .trim() // Remove whitespace from both ends of a string
     .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/&/g, "-and-") // Replace & with 'and'  
     .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
     .replace(/\-\-+/g, "-"); // Replace multiple - with single -
 }
